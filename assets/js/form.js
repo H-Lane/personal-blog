7b70e0fd5b8cbde768b64dc786c259db7da7b5fc
Hersey <helane1996@gmail.com>
2024-03-25 18:27:39 -0400

    added in some js values to keep track of the user inputs and matched them with appropriate divs on the index.html

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,17 @@
-let user = document.querySelector(`#user`)
-let title = document.querySelector(`#title`)
-let content = document.querySelector(`#content`)
+let user = document.querySelector(`#user`);
+let title = document.querySelector(`#title`);
+let content = document.querySelector(`#content`);
+
+let userValue = localStorage.getItem(userText);
+let titleValue = localStorage.getItem(titleText);
+let contentValue = localStorage.getItem(contentText);
+
+if (userValue == null) {
+  let userText;
+
+  userText = document.createTextNode("Input your username here");
+} else {
+  userText = document.createTextNode(userValue);
+}
+
+user.appendChild(userText);
