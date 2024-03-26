7049949106191f98495b3e6db10b8e3712e99f81
Hersey <helane1996@gmail.com>
2024-03-25 21:57:23 -0400

    added input boxes to the html with refs in the css and js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,9 @@
-let user = document.querySelector(`#user`);
-let title = document.querySelector(`#title`);
-let content = document.querySelector(`#content`);
+let userEl = document.querySelector(`#user`);
+let titleEl = document.querySelector(`#title`);
+let contentEl = document.querySelector(`#content`);
 
-let userValue = localStorage.getItem(userText);
-let titleValue = localStorage.getItem(titleText);
-let contentValue = localStorage.getItem(contentText);
+// let userValue = localStorage.getItem(userText);
+// let titleValue = localStorage.getItem(titleText);
+// let contentValue = localStorage.getItem(contentText);
 
-if (userValue == null) {
-  let userText;
 
-  userText = document.createTextNode("Input your username here");
-} else {
-  userText = document.createTextNode(userValue);
-}
-
-user.appendChild(userText);
