79aefc8b332b95d05e6002a271387c3f13e7d6a4
Hersey <helane1996@gmail.com>
2024-03-26 16:56:01 -0400

    added some js to change the redirect from the html to the js. added in a warning if the user does not fill in all fields before hitting submit

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,21 @@
 const userEl = document.getElementById(`user`);
 const titleEl = document.getElementById(`title`);
 const contentEl = document.getElementById(`content`);
-const submitEl = document.getElementById(`submit-button`)
+const submitEl = document.getElementById(`submit-button`);
 
 submitEl.addEventListener(`click`, function (event) {
   event.preventDefault();
+
+  if (userEl.value.length == 0) {
+    errorMessage();
+    return;
+  } else if (titleEl.value.length == 0) {
+    errorMessage();
+    return;
+  } else if (contentEl.value.length == 0) {
+    errorMessage();
+    return;
+  }
 
   const blogPost = {
     user: userEl.value,
@@ -14,45 +25,23 @@ submitEl.addEventListener(`click`, function (event) {
 
   localStorage.setItem(`blogPost`, JSON.stringify(blogPost));
 
+  userEl.value = "";
+  titleEl.value = "";
+  contentEl.value = "";
 
-  userEl.value = ("")
-  titleEl.value = ("")
-  contentEl.value = ("")
+  window.location.href = `./blog.html`
 
+  //TODO: Add in a redirect to the blog.html page and find a way to automatically make the blogPost object populate into a created div on that new page
+});
+
+function errorMessage() {
+  alert("Please complete all empty forms before submitting.");
   
-//TODO: Add in a redirect to the blog.html page and find a way to automatically make the blogPost object populate into a created div on that new page
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // if (alert) {
+  //   return
+  // }
+}
 
 // let userValue = localStorage.getItem(userText);
 // let titleValue = localStorage.getItem(titleText);
 // let contentValue = localStorage.getItem(contentText);
-
-
