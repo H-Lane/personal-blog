435692e8f6e6b7e30d7e6f88a931dc56a847f205
Hersey <helane1996@gmail.com>
2024-03-26 00:45:18 -0400

    created the script necessary to store the username, title, and content into the local storage for use on the display page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,56 @@
-let userEl = document.querySelector(`#user`);
-let titleEl = document.querySelector(`#title`);
-let contentEl = document.querySelector(`#content`);
+const userEl = document.getElementById(`user`);
+const titleEl = document.getElementById(`title`);
+const contentEl = document.getElementById(`content`);
+const submitEl = document.getElementById(`submit-button`)
+
+submitEl.addEventListener(`click`, function (event) {
+  event.preventDefault();
+
+  const blogPost = {
+    user: userEl.value,
+    title: titleEl.value,
+    content: contentEl.value.trim(),
+  };
+
+  localStorage.setItem(`blogPost`, JSON.stringify(blogPost));
+
+
+// const newBlogPost = JSON.parse(localStorage.getItem('blogPost'));
+
+  userEl.value = ("")
+  titleEl.value = ("")
+  contentEl.value = ("")
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let userValue = localStorage.getItem(userText);
 // let titleValue = localStorage.getItem(titleText);
